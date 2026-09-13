e4d24c445edaf4b245e1a6db67e8685565d233e1
Akashkushwah23654 <162025716+Akashkushwah23654@users.noreply.github.com>
2026-09-14 00:01:06 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,22 @@
+var CACHE = 'tetrisneon-v7';
+var FILES = ['./', './index.html', './game.js', './audio.js', './manifest.webmanifest',
+  './icons/icon-192.png', './icons/icon-512.png', './icons/icon-512-maskable.png'];
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open(CACHE).then(function (c) { return c.addAll(FILES); }).then(function () { return self.skipWaiting(); }));
+});
+self.addEventListener('activate', function (e) {
+  e.waitUntil(caches.keys().then(function (ks) {
+    return Promise.all(ks.map(function (k) { return k === CACHE ? null : caches.delete(k); }));
+  }).then(function () { return self.clients.claim(); }));
+});
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') return;
+  if (new URL(e.request.url).origin !== self.location.origin) return;
+  e.respondWith(caches.match(e.request).then(function (hit) {
+    return hit || fetch(e.request).then(function (res) {
+      var copy = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+      return res;
+    }).catch(function () { return caches.match('./index.html'); });
+  }));
+});
